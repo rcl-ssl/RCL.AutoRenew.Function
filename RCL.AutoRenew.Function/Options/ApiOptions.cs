9ab628ec23ae267d2f28499be395975981abbecf
Ray Consulting <rayconsulting@shopeneur.com>
2022-04-30 06:51:11 -0400

    changed subscription Id

--- RCL.AutoRenew.Function/Options/ApiOptions.cs
+++ RCL.AutoRenew.Function/Options/ApiOptions.cs
@@ -5,6 +5,6 @@ namespace RCL.AutoRenew.Function
     public class ApiOptions
     {
         public string ApiEndpoint { get; set; }
-        public int SubscriptionId { get; set; }
+        public string SubscriptionId { get; set; }
     }
 }
